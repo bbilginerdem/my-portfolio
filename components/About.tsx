9849580cd1291ae0edc71401d0904593810c4d9d
Behzat Bilgin Erdem <e200456@metu.edu.tr>
2023-10-16 14:38:37 +0300

    added header along with about text and fixed divider a bit

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
+"use client";
 
-function About() {
+import React from "react";
+import SectionHeading from "@/components/SectionHeader";
+import { motion } from "framer-motion";
+// import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+// const { ref } = useSectionInView("About");
+
   return (
-    <section>
-      <h2>
-        About me
-      </h2>
-    </section>
-  )
-}
+    <motion.section
+      // ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Physics</span>, I decided to pursue my
+        passion for programming. I started working in a company as an intern and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, React Native, Expo, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a frontend
+        developer.
+      </p>
 
-export default About
+      <p>
+        <span className="italic">When I&apos;m not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I&apos;m also
+        learning how to play the guitar.
+      </p>
+    </motion.section>
+  );
+}
