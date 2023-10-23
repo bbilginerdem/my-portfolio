7dc6e165dd3808cf91fbb305565468c54b8a7277
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2023-10-23 19:09:29 +0300

    section header done

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 // import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-// const { ref } = useSectionInView("About");
+  // const { ref } = useSectionInView("About");
 
   return (
     <motion.section
@@ -37,12 +37,11 @@ export default function About() {
       </p>
 
       <p>
-        <span className="italic">When I&apos;m not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="italic">When I&apos;m not coding</span>, I enjoy juggling, lindy hop, and playing with my cats. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I&apos;m also
-        learning how to play the guitar.
+        <span className="font-medium">psychology and philosophy</span>. I&apos;m also
+        learning how to do 3D animations on mobile and web.
       </p>
     </motion.section>
   );
---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionHeading from './SectionHeader'
+
+function Projects() {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Projects
