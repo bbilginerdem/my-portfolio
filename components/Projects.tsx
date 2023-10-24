cd54ae6e4dc2f073891ec628572dff8063f39aec
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2023-10-24 12:42:43 +0300

    project component done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import SectionHeading from './SectionHeader'
+import { projectsData } from '@/lib/data'
+import Project from './Project'
 
 function Projects() {
   return (
     <section>
       <SectionHeading>My Projects</SectionHeading>
       <div>
-
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))
+        }
       </div>
     </section>
   )
