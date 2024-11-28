6d6b22317534dfe7c791305ee97383c447b39122
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2024-11-28 18:47:49 +0300

    NCU & deleted cringe content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
@@ -11,10 +9,8 @@ export default function Home() {
         <main className="flex flex-col items-center px-4">
             <Intro />
             <SectionDivider />
-            <About />
             <Projects />
             <Skills />
-            <Experience />
             <Contact />
         </main>
     );
